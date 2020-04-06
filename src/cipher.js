de84f0860a4a6efade69b332b827dac6afab5e60
Thais Durynek <thais.durynek@gmail.com>
2020-04-06 01:52:26 -0300

    Função encode e decode feitas no index

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,17 @@
 const cipher = {
-    /*function: cipher.encode(inputText, inputOffset) {
-        for(let i = 0; i < stringLength; i++){
-        ([inputText].charCodeAt(i) + inputOffset)% 26
+    /*encode: function(){ 
+        for (let i=0; i < stringLength; i++) {
+            let calcEncode = ((inputText.charCodeAt(i) - 65 + inputOffset)% 26 + 65);
+            let cifrado = String.fromCharCode(calcEncode);
+            document.write(cifrado);
+          } 
+    }, 
+
+    decode: function(){
+        for (let cont=0; cont < stringLength; cont++) {
+            let calcDecode = ((inputText.charCodeAt(cont) - 65 - inputOffset)% 26 + 65);
+            let decifrado = String.fromCharCode(calcDecode);
+            document.write(decifrado);
         }
     } */
 };
--- src/index.js
+++ src/index.js
@@ -2,41 +2,33 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-
-                /* BOTÃO DO TEXTO */
-let textButton = document.getElementById("buttonText"); //peguei o botao do HTML e add um eventListener que chama a função text ao clicar
-textButton.addEventListener('click', text)
-
-function text(){
-    let inputText = document.getElementById("message").value; //criei uma função que pega o valor da tag
-    let stringLenght = inputText.length
-    alert("O tamanho da sua string é " + stringLenght)
-}
-
-
-                /* BOTÃO DO DESLOCAMENTO */
-let textOffset = document.getElementById("buttonOffset");
-textOffset.addEventListener('click', offset)
-
-function offset(){
-    let inputOffset = document.getElementById("offset").value;
-    alert(inputOffset);
-}
-
                 /* BOTÃO DE CIPHER E DECIPHER */
 /* Criei duas variáveis para pegar os elementos botões de Cifrar e Decifrar.
 Utilizei as variáveis para fazer um eventListener. Dentro de cada função eu vou
 tentar chamar as funções cipher e decipher no chipher.js */
-           
+   
 let buttonCipher = document.getElementById("buttonOne");
+buttonCipher.addEventListener('click', function cipherCall(){
+    let inputText = document.getElementById("message").value;
+    let stringLength = Number(inputText.length);
+    let inputOffset = Number(document.getElementById("offset").value);
+    for (let i=0; i < stringLength; i++) {
+        let calcEncode = ((inputText.charCodeAt(i) - 65 + inputOffset)% 26 + 65);
+        let cifrado = String.fromCharCode(calcEncode);
+        document.write(cifrado); 
+      }
+});
+
 let buttonDecipher = document.getElementById("buttonTwo");
-buttonCipher.addEventListener('click', function cipher(){
-    let cifrarBotao = document.write("você escolheu cifrar");
-    return cifrarBotao;
-})
-buttonDecipher.addEventListener('click', function decipher(){
-    let decifrarBotao = document.write("você escolheu decifrar");
-    return decifrarBotao;
+buttonDecipher.addEventListener('click', function decipherCall(){
+    let inputText = document.getElementById("message").value;
+    let stringLength = Number(inputText.length);
+    let inputOffset = Number(document.getElementById("offset").value);
+    for (let cont=0; cont < stringLength; cont++) {
+        let calcDecode = ((inputText.charCodeAt(cont) - 65 - inputOffset)% 26 + 65);
+        let decifrado = String.fromCharCode(calcDecode);
+        document.write(decifrado); 
+      }
 })
 
 /* usar animação para a caixa de resposta (easy out) */
