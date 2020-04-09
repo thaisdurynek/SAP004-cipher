9174b0add305c8e712f107733ad3817db6805d5a
Thais Durynek <thais.durynek@gmail.com>
2020-04-08 21:54:33 -0300

    Versão em que os testes passaram

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,23 @@
 const cipher = {
-    cifrar: function cifrar(inputText, inputOffset) {
+    encode: function encode(inputOffset, inputText) { //função recebe parâmetros do index.js
+        let cipherUpperCase = inputText.toUpperCase(); //declaração de variáveis fora do laço 
         let cifrado = ""; 
         let calcEncode = 0;
-        for (let i=0; i < Number(inputText.length); i++) {
-            calcEncode = ((inputText.charCodeAt(i) - 65 + inputOffset)% 26 + 65);
+        for (let i=0; i < cipherUpperCase.length; i++) { //laço que utiliza um contador que é incrementado até alcan
+            calcEncode = ((cipherUpperCase.charCodeAt(i) - 65 + inputOffset)% 26 + 65);
             cifrado += String.fromCharCode(calcEncode);
           } 
-        //alert(`Seu bilhete decifrado é ${cifrado}`);
         return cifrado;
     }, 
 
-    decifrar: function decifrar(inputText, inputOffset) {
+    decode: function decode(inputOffset, inputText) {
+        let decipherUpperCase = inputText.toUpperCase();
         let decifrado = "";
         let calcDecode = 0;
-        for (let cont=0; cont < Number(inputText.length); cont++) {
-            calcDecode = ((inputText.charCodeAt(cont) - 90 - inputOffset)% 26 + 90);
+        for (let cont=0; cont < decipherUpperCase.length; cont++) {
+            calcDecode = ((decipherUpperCase.charCodeAt(cont) - 90 - inputOffset)% 26 + 90);
             decifrado += String.fromCharCode(calcDecode);
         }
-        console.log(calcDecode);
-        //alert(`Seu bilhete decifrado é ${decifrado}`);
         return decifrado;
     } 
 };
--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import cipher from './cipher.js';
 
-                /* BOTÃO DE CIPHER E DECIPHER */
-/* Criei duas variáveis para pegar os elementos botões de Cifrar e Decifrar.
-Utilizei as variáveis para fazer um eventListener. Dentro de cada função eu vou
-tentar chamar as funções cipher e decipher no chipher.js */
+                /* Manipulação de elementos DOM */
 
-
+/*Criei as variáveis e peguei os elementos HTML por ID*/
 let inputText = document.getElementById("text");
 let inputOffset = document.getElementById("offset");
 let buttonCipher = document.getElementById("buttonOne");
 let buttonDecipher = document.getElementById("buttonTwo");
 
-buttonCipher.addEventListener("click", callEncode);
-buttonDecipher.addEventListener("click", callDecode);
-
+/*Funções que chamam o objeto cipher e colocam o return dentro do HTML*/
 function callEncode() {
-    document.getElementById("resposta").innerHTML = cipher.cifrar(inputText.value, Number(inputOffset.value));
+    document.getElementById("resposta").innerHTML = cipher.encode(Number(inputOffset.value), inputText.value);
 }
 
 function callDecode() {
-    document.getElementById("resposta").innerHTML = cipher.decifrar(inputText.value, Number(inputOffset.value));
+    document.getElementById("resposta").innerHTML = cipher.decode(Number(inputOffset.value), inputText.value);
 }
-   
+
+/*Evento que é disparado ao clicar nos botões*/
+buttonCipher.addEventListener("click", callEncode);
+buttonDecipher.addEventListener("click", callDecode);
+
+
 
 /* usar animação para a caixa de resposta (easy out) */
 //keyframe animação css
